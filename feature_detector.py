b9ed299082fed109c6dd23cf460dcaed260a5f2a
EK <a@a.com>
2021-02-19 21:26:13 +0000

    working plot of matched keypoints

--- feature_detector.py
+++ feature_detector.py
@@ -35,7 +35,7 @@ class FeatureDetector:
         self.des.append(des)
         self.imgs.append(img)
 
-        self.show(img, kps)
+        #self.show(img, kps)
 
         #print('len self.imgs', len(self.imgs))
 
@@ -44,6 +44,28 @@ class FeatureDetector:
         cv2.drawKeypoints(img, kps, img, color=(0,255,0), flags=0)
         #cv2.drawKeypoints(img, list(self.kps), img, color=(0,255,0), flags=0)
         #cv2.imshow('Matches', img)
+
+    def show_matches(self, matches, kps, img):
+        '''
+        What is this Matcher Object?
+        The result of matches = bf.match(des1,des2) line is a list of DMatch objects. 
+        This DMatch object has following attributes:
+          DMatch.distance - Distance between descriptors. The lower, the better it is.
+          DMatch.trainIdx - Index of the descriptor in train descriptors
+          DMatch.queryIdx - Index of the descriptor in query descriptors
+          DMatch.imgIdx - Index of the train image.
+        '''
+      
+        kps_to_draw_t, kps_to_draw_q = [], []
+        for m in matches:
+            # extract matched keypoint and save it
+            kps_to_draw_t.append(kps[m.trainIdx])
+            kps_to_draw_q.append(kps[m.queryIdx])
+
+        img_out = cv2.drawKeypoints(img, kps_to_draw_t, img, color=(255, 0, 0), flags=0)
+        img_out = cv2.drawKeypoints(img_out, kps_to_draw_q, img_out, color=(0, 0, 255), flags=0)
+
+        return img_out
 
     def match(self):
         ''' perform keypoint matching from the data stored in the detector
@@ -63,15 +85,22 @@ class FeatureDetector:
 
             matches = bf.match(des1, des2)
 
+            #print('imgIdx: ', matches[0].imgIdx)
+
             # sort in order of distance
             matches = sorted(matches, key = lambda x: x.distance)
+
+            # display matched keypoints on the current image
+            time.sleep(0.2)
+            img_out = self.show_matches(matches, kps2,  img2)
 
             #print('num matches: ', len(matches))
             #print('i: ', i)
             #print(self.imgs)
+            '''
             img_out = cv2.drawMatches(img1, kps1, img2, kps2, matches[:40],
                                       None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
+            '''
             
             cv2.imshow('InMatch', img_out)
             key = cv2.waitKey(1) & 0xFF           
